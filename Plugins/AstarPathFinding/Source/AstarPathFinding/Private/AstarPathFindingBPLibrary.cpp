f7750a2a4427cc7cb715e10230a50313887c7df7
damien220 <ashrafalnas@hotmail.com>
2025-01-16 06:08:04 +0200

    first commit of working algorithm

---
+++ Plugins/AstarPathFinding/Source/AstarPathFinding/Private/AstarPathFindingBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "AstarPathFindingBPLibrary.h"
+#include "AstarPathFinding.h"
+
+UAstarPathFindingBPLibrary::UAstarPathFindingBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UAstarPathFindingBPLibrary::AstarPathFindingSampleFunction(float Param)
+{
+	return -1;
+}
+
